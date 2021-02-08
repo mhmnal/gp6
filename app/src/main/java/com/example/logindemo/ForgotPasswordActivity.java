4ac81f0ebc5aee997244909de88786859fd4e8b6
DESKTOP-HHR49PH\mhmni <mhmn.inabox@gmail.com>
2021-02-08 19:31:49 +0800

    create forgot password

---
+++ app/src/main/java/com/example/logindemo/ForgotPasswordActivity.java
@@ -0,0 +1,14 @@
+package com.example.logindemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
