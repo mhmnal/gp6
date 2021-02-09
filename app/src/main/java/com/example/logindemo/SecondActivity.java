27b8ce68dc432877bbb334ad21dd4895b4042706
DESKTOP-HHR49PH\mhmni <mhmn.inabox@gmail.com>
2021-02-09 18:56:13 +0800

    include action bar

--- app/src/main/java/com/example/logindemo/SecondActivity.java
+++ app/src/main/java/com/example/logindemo/SecondActivity.java
@@ -25,9 +25,7 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_second);
 
         firebaseAuth = FirebaseAuth.getInstance();
