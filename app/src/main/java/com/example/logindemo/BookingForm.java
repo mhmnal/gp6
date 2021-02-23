6ac8b93bdb6f94c03be96740310df1ca44ea86fe
FAREES <fareesnazmi2@gmail.com>
2021-02-24 00:04:22 +0800

    Design interface booking form

---
+++ app/src/main/java/com/example/logindemo/BookingForm.java
@@ -0,0 +1,14 @@
+package com.example.logindemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookingForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking_form);
+    }
+}
